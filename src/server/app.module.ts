5f82de1e51a0c01f2198ee833a530c46b405862b
Nick Sharp <nick.sharp.87@gmail.com>
2023-01-06 15:29:27 +0000

    chore: update app-module

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { RenderModule } from 'nest-next';
 import Next from 'next';
 import { join, resolve } from 'path';
+import { DeviceController } from './controllers/devices';
 import { PagesController } from './controllers/pages';
-
-import { Group } from './data/models/group.entity';
-import { Project } from './data/models/project.entity';
+import databaseConfig from './data/database.config';
+import { DeviceService } from './lib/device.service';
 
 const IS_DEV = process.env.NODE_ENV === 'development';
-
-const REPOSITORY_MODELS = [Project, Group];
 
 @Module({
   imports: [
@@ -29,23 +27,15 @@ const REPOSITORY_MODELS = [Project, Group];
         dev: IS_DEV,
       },
     ),
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      host: 'localhost',
-      port: 27017,
-      username: 'root',
-      password: 'root',
-      database: 'mission-control',
-      authSource: 'admin',
-      entities: REPOSITORY_MODELS,
-      synchronize: IS_DEV,
-    }),
-    TypeOrmModule.forFeature(REPOSITORY_MODELS),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'public'),
     }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+      isGlobal: true,
+    }),
   ],
-  controllers: [PagesController],
-  providers: [],
+  controllers: [PagesController, DeviceController],
+  providers: [DeviceService],
 })
 export class AppModule {}
